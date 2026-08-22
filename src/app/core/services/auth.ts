24e37c40d44b199cc5ce46277b2ace1623ff91cb
jdev <juanj@jdev.com>
2026-08-21 18:14:53 -0600

    remove localstorage,  rename roles name

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -6,15 +6,12 @@ import { UserMe } from '../models/auth.models';
 import { Observable, tap } from 'rxjs';
 
 export type UserRole =
-  | 'PUBLIC'
-  | 'CUSTOMER'
-  | 'EMPLOYEE'
-  | 'SALON_ADMIN';
+  | 'ROLE_PUBLIC'
+  | 'ROLE_CUSTOMER'
+  | 'ROLE_EMPLOYEE'
+  | 'ROLE_SALON_ADMIN';
 
-export const bs_roles = 'bs_roles';
-export const bs_token = 'bs_token';
-export const bs_name = 'bs_name';
-export const bs_user = 'bs_user';
+
 export const TOKEN_STORAGE_KEY = 'bs_last_pushed_fcm_token';
 
 
@@ -35,17 +32,15 @@ export class AuthService {
   private readonly http = inject(HttpClient);
   private readonly appConfig = inject(AppConfigService);
 
-  private currentRolesSignal = signal<UserRole[]>(
-    this.loadRoles()
-  );
 
-  private currentUserNameSignal = signal<string>(
-    localStorage.getItem(bs_name) ?? ''
-  );
+  public accessToken = signal<string | null>(null);
 
-  private currentUserSignal = signal<UserMe | null>(
-    this.loadUser()
-  );
+
+  private currentRolesSignal = signal<UserRole[]>(['ROLE_PUBLIC']);
+
+  private currentUserNameSignal = signal<string>('');
+
+  private currentUserSignal = signal<UserMe | null>(null);
   
 
   readonly currentUser = this.currentUserSignal.asReadonly();
@@ -53,23 +48,52 @@ export class AuthService {
 
 
   login(data: any) {
-    return this.http.post<{token: string}>(`${this.appConfig.apiUrl}/api/v1/auth/login`, data);
+    return this.http.post<{token: string}>(`${this.appConfig.apiUrl}/api/v1/auth/login`, data, {withCredentials: true})
+    .pipe(
+      tap((response) => {
+
+        this.setToken(response.token);
+        
+      })
+    );
+
   }
-  
 
-  private loadRoles(): UserRole[] {
-    const value = localStorage.getItem(bs_roles);
+  refresh(): Observable<any> {
+    return this.http.post<any>(`${this.appConfig.apiUrl}/api/v1/auth/refresh`, {}, { withCredentials: true })
+    .pipe(
+      tap((response) => {
 
-    if (!value) {
-      return ['PUBLIC'];
-    }
-
-    try {
-      return JSON.parse(value) as UserRole[];
-    } catch {
-      return ['PUBLIC'];
-    }
+        this.setToken(response.token);
+        
+      })
+    );
   }
+
+  setToken(token: string) {
+
+    const payload = jwtDecode<JwtPayload>(token);
+
+    this.currentUserNameSignal.set(payload.name)
+    this.setRoles(payload.roles);
+
+    this.accessToken.set(token);
+
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.appConfig.apiUrl}/api/v1/auth/logout`, {}, { withCredentials: true })
+    .pipe(
+      tap(() => {
+        
+        this.accessToken.set(null);
+
+        this.currentRolesSignal.set(['ROLE_PUBLIC']);
+        
+      })
+    )
+  }
+
 
   loadAuthenticatedUser(): Observable<UserMe> {
 
@@ -77,11 +101,6 @@ export class AuthService {
       tap(user => {
   
         this.currentUserSignal.set(user);
-  
-        localStorage.setItem(
-          bs_user,
-          JSON.stringify(user)
-        );
   
       })
     );
@@ -99,30 +118,11 @@ export class AuthService {
     ).pipe(
       tap(user => {
         
-        localStorage.setItem(
-          bs_user,
-          JSON.stringify(user)
-        );
-
         this.currentUserSignal.set(user);
       })
     );
   }
 
-  private loadUser(): UserMe | null {
-
-    const value = localStorage.getItem(bs_user);
-
-    if (!value) {
-      return null;
-    }
-
-    try {
-      return JSON.parse(value);
-    } catch {
-      return null;
-    }
-  }
 
 
   updateUserProfilePicture(newPictureUrl: string): void {
@@ -135,8 +135,6 @@ export class AuthService {
         pictureUrl: newPictureUrl
       };
 
-      // 2. Guardar la versión actualizada en LocalStorage
-      localStorage.setItem(bs_user, JSON.stringify(updatedUser));
 
       return updatedUser;
     });
@@ -145,7 +143,7 @@ export class AuthService {
 
 
   isLoggedIn(): boolean {
-    return !this.hasRole('PUBLIC');
+    return !this.hasRole('ROLE_PUBLIC');
   }
 
   getRoles(): UserRole[] {
@@ -164,29 +162,12 @@ export class AuthService {
     return roles.some(role => this.hasRole(role));
   }
 
-  saveToken(token: string): void {
 
-    const payload = jwtDecode<JwtPayload>(token);
-    
-    localStorage.setItem(bs_token, token);
-    localStorage.setItem(bs_name, payload.name);
-    localStorage.setItem(bs_roles, JSON.stringify(payload.roles));
 
-    this.currentUserNameSignal.set(payload.name)
-    this.setRoles(payload.roles);
-
-  }
-
-  logout(): void {
-    localStorage.removeItem(bs_token);
-    localStorage.removeItem(bs_roles);
-
-    this.setRoles(['PUBLIC']);
-  }
 
   setRoles(roles: UserRole[]): void {
     this.currentRolesSignal.set(
-      roles.length ? roles : ['PUBLIC']
+      roles.length ? roles : ['ROLE_PUBLIC']
     );
   }
 }
