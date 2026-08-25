6fb82757fa0578878e7bd175f6478fbfa080e410
jdev <juanj@jdev.com>
2026-08-25 13:37:07 -0600

    evaluete hasSeenBookingTooltip

--- src/app/features/customer/service/cus-booking.ts
+++ src/app/features/customer/service/cus-booking.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { AppConfigService } from '../../../config/app-config.service';
 import { ResponseCustommerBooking } from '../models/cus-booking.models';
-import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +14,14 @@ export class CustomerBookingService {
 
 
   private activeCountSignal = signal<number>(0);
+  
+
     
   readonly activeCount = this.activeCountSignal.asReadonly();
   readonly hasActive = computed(() => this.activeCountSignal() > 0);
+  
+  private bookingTooltipSignal = signal<boolean>(false);
+  readonly showBookingTooltip = this.bookingTooltipSignal.asReadonly();
 
 
   bookings(mode: 'active' | 'history', page: number, size: number) {
@@ -29,12 +33,36 @@ export class CustomerBookingService {
     return this.http.post<void>(`${this.appConfig.apiUrl}/api/v1/customer/bookings`, data)
   }
 
-  fetchCountActiveBookings() {
-    return this.http.get<{bookingCount:number}>(`${this.appConfig.apiUrl}/api/v1/customer/bookings/actives`)
+  fetchCountActiveBookings(): void {
+    this.http.get<{bookingCount:number}>(`${this.appConfig.apiUrl}/api/v1/customer/bookings/actives`)
     .subscribe({
-      next: (value) => this.activeCountSignal.set(value.bookingCount ?? 0),
+      next: (value) => {
+
+        let bookingCount = value.bookingCount ?? 0;
+
+        this.activeCountSignal.set(bookingCount);
+
+        if(bookingCount > 0) {
+
+          const hasSeenTooltip = localStorage.getItem('hasSeenBookingTooltip');
+          
+          if (!hasSeenTooltip) {
+            
+            this.bookingTooltipSignal.set(true);
+          }
+
+
+
+        }
+
+      },
       error: (err) => console.error('Error al obtener reservaciones activas', err)
     })
+  }
+
+  dismissBookingTooltip() {
+    this.bookingTooltipSignal.set(false);
+    localStorage.setItem('hasSeenBookingTooltip', 'true');
   }
   
   
